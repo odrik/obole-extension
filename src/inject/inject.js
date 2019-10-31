b57f97c694e042d4894e6feb0e32f5e70ba0cb39
Audric <odrikm@hotmail.fr>
2019-10-31 21:34:13 +0100

    edit extension

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,15 +2,12 @@
 
 $(document).ready(init);   // Content Ready
 
-var RETRY_DELAY_MS = 800;  // Time between retry
-var MAX_RETRY = 2;		   // Number of retry
-
 var timer,
-	retryCount = 0,
 	options = {},
 	find = false,
 	link = '',
 	layout = '2015',
+	social = 'youtube',
 	$buttonContainer = null;
 
 function init() {
@@ -18,71 +15,101 @@ function init() {
 
 	clearTimeout(timer);
 	timer = setTimeout(function() {
-		//console.log("[INFO] : Testing…");
+		$('.obole-container').html('');
+		console.log("[INFO] : Testing…");
 
-		$channel = $('body').find('a[href*="/channel/"]').first().attr("href").split("/").pop();
-		$user = $('body').find('a[href*="/user/"]').first().attr("href").split("/").pop();
+		$channel = $('body').find('a[href*="/channel/"]').first().attr("href");
 
-		console.log('CHANNEL : ' + $channel);
-		console.log('USER : ' + $user);
+		if ($channel) {
+			$channel = $channel.split("/").pop();
+			console.log('[INFO] : Channel ID ' + $channel);
+			$.ajax({
+			  	method: "POST",
+			  	url: "https://odrik.fr/public/index.php/api/search",
+				data: {
+					social: social,
+					search: $channel
+				}
+			}).done(function(msg) {
+				console.log(msg);
+				if (msg.success) {
+					link = msg.link;
+					find = true;
+		
+					setLayout();
+					//console.log("[INFO] : layout version " + layout);
+					
+					var utipButton = '<button class="obole-button"><svg role="img" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5 6.34884C12.2368 6.34884 6.34884 12.2368 6.34884 19.5C6.34884 26.7632 12.2368 32.6512 19.5 32.6512C26.7632 32.6512 32.6512 26.7632 32.6512 19.5C32.6512 12.2368 26.7632 6.34884 19.5 6.34884ZM0 19.5C0 8.73045 8.73045 0 19.5 0C30.2696 0 39 8.73045 39 19.5C39 30.2696 30.2696 39 19.5 39C8.73045 39 0 30.2696 0 19.5Z" fill="url(#paint0_linear)"/> <defs> <linearGradient id="paint0_linear" x1="2.2694" y1="-6.05172" x2="39" y2="36.7726" gradientUnits="userSpaceOnUse"> <stop offset="0.287293" stop-color="#E14479"/> <stop offset="0.823204" stop-color="#314CDE"/> </linearGradient> </defs> </svg></button>';
+					
+					var button = '<div class="obole-container">'+utipButton+'</div>';
+					
+					if ($('#iri-quick-controls-container').length > 0) {
+						$('#iri-quick-controls-container').append(utipButton);
+					} else if (layout == '2018') {
+						$buttonContainer.before(button);
+					} else if (layout == '2015') {
+						$buttonContainer.prepend(button);
+						$('body').addClass('obole-layout-2015');
+						$('.obole-container').addClass('old-layout');
+					} else {
+						$buttonContainer.prepend(button);
+						$('.obole-container').addClass('old-layout');
+					}
 
-		// if ($channel != null && $user != null) {
-		// 	find = true
-		// }
+					
+					$('.obole-button').on("click", openLink);
 
-		$.ajax({
-		  	method: "POST",
-		  	url: "https://odk-group.fr/json.php",
-			data: {
-				name: "John"
-			}
-		}).done(function(msg) {
-			console.log(msg[0].page);
-			if (msg[0].page != undefined) {
-				link = msg[0].page;
-				find = true
-			}
-		});
+					// var sb_btn = document.createElement('div');
+     //                sb_btn.id = "obole-tab";
+     //                sb_btn.innerHTML = `
+     //                <style>	
+     //                		a {
+     //                			text-decoration: none;
+     //                		}
+     //                        #socialblade-tab paper-button {
+     //                            background-color: #2a41e8 !important;
+     //                            border-radius: 2px;
+     //                            color: #ffffff !important;
+     //                            padding: 10px 16px;
+     //                            margin: auto 4px;
+     //                            white-space: nowrap;
+     //                            font-size: 1.4rem;
+     //                            font-weight: 500;
+     //                            letter-spacing: .007px;
+     //                            text-transform: uppercase;
+     //                            display: flex;
+     //                            -ms-flex-direction: row;
+     //                            -webkit-flex-direction: row;
+     //                            flex-direction: row;
+     //                            box-shadow: none!important;
+     //                        }
+     //                        #socialblade-tab paper-button img {
+     //                            width: 25px;
+     //                            padding-right: 10px;
+     //                            padding-bottom: 2px;
+     //                        }
+     //                    </style>
+     //                    <a href="${link}"><paper-button role="button" tabindex="0" animated="" aria-disabled="false" elevation="O" raised="" id="button" class="style-scope ytd-button-renderer style-default">
+     //                        SOUTENIR SUR OBOLE
+     //                        <paper-ripple class="style-scope paper-button">
+     //                            <div id="background" class="style-scope paper-ripple"></div>
+     //                            <div id="waves" class="style-scope paper-ripple"></div>
+     //                        </paper-ripple>
+     //                    </paper-button></a>`;
+     //                if (document.getElementById("obole-tab") == null) document.getElementById("channel-header-container").insertBefore(sb_btn, document.getElementById("channel-header-container").children[3]);
 
-		if (find) {
-			setLayout();
-			console.log("[INFO] : utip link found");
-			console.log("[INFO] : layout version " + layout);
-			
-			var utipButton = '<button class="obole-button"><svg role="img" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5 6.34884C12.2368 6.34884 6.34884 12.2368 6.34884 19.5C6.34884 26.7632 12.2368 32.6512 19.5 32.6512C26.7632 32.6512 32.6512 26.7632 32.6512 19.5C32.6512 12.2368 26.7632 6.34884 19.5 6.34884ZM0 19.5C0 8.73045 8.73045 0 19.5 0C30.2696 0 39 8.73045 39 19.5C39 30.2696 30.2696 39 19.5 39C8.73045 39 0 30.2696 0 19.5Z" fill="url(#paint0_linear)"/> <defs> <linearGradient id="paint0_linear" x1="2.2694" y1="-6.05172" x2="39" y2="36.7726" gradientUnits="userSpaceOnUse"> <stop offset="0.287293" stop-color="#E14479"/> <stop offset="0.823204" stop-color="#314CDE"/> </linearGradient> </defs> </svg></button>';
-			
-			var button = '<div class="obole-container">'+utipButton+'</div>';
-			
-			if ($('#iri-quick-controls-container').length > 0) { // Check for Iridium compatibility - https://github.com/ParticleCore/Iridium
-				$('#iri-quick-controls-container').append(utipButton);
-			} else if (layout == '2018') {
-				$buttonContainer.before(button);
-			} else if (layout == '2015') {
-				$buttonContainer.prepend(button);
-				$('body').addClass('obole-layout-2015');
-				$('.obole-container').addClass('old-layout');
-			} else {
-				$buttonContainer.prepend(button);
-				$('.obole-container').addClass('old-layout');
-			}
-		} else {
-			if (retryCount < MAX_RETRY) {
-				retryCount++;
-				//console.log("[INFO] : retry ("+retryCount+")");
-				init();
-			}
+				}
+			});
 		}
-
-		$('.obole-button').on("click", openUtip);
 
 		if(options.thumbUp) {
-			$('#top-level-buttons ytd-toggle-button-renderer:first-child > a, .like-button-renderer-like-button').on("click", openUtip);
+			$('#top-level-buttons ytd-toggle-button-renderer:first-child > a, .like-button-renderer-like-button').on("click", openLink);
 		}
 
-	}, RETRY_DELAY_MS);
+	}, 1000);
 };
 
-function openUtip() {
+function openLink() {
 	if (find) {
 		window.open(link, "popupWindow", "width=600,height=600,scrollbars=yes");
 	}
@@ -101,7 +128,7 @@ function setLayout() {
 			$buttonContainer = $buttonContainer.find('ytd-menu-renderer');
 		}
 	} else if ($('ytg-watch-footer').length > 0) {
-		// 2016 Gaming
+		// 2016
 		layout = '2016';
 		$buttonContainer = $('ytg-watch-footer');
 		if ($buttonContainer.find('.actions').length > 0) {
@@ -121,7 +148,6 @@ function reset() {
 function refresh() {
 	console.log("[INFO] : looking for a link…");
 	reset();
-	retryCount = 0;
 	$(document).ready(init);
 };
 
@@ -137,7 +163,6 @@ function getOptions() {
 var isLoaded = false;
 
 chrome.extension.onMessage.addListener(function(request, sender, response) {
-	// listen to messages sent by the background script
 	console.log("[MSG] : ", request.type);
 	if (request.type === 'complete') {
 		refresh();
